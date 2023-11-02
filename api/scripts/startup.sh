7854eb119535dea3938679fac06f126299471afd
Mike <59815217+MFinn87@users.noreply.github.com>
2023-11-02 15:08:53 -0600

    Added hello world to API (#2)

---
+++ api/scripts/startup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# TODO: Use proper pg_wait
+sleep 5
+
+echo "Starting up..."
+
+if [ "$SHOULD_RUN_DATABASE_MIGRATE" = "true" ]; then
+  echo "Running database migrations..."
+  yarn migrate
+fi
+
+if [ "$SHOULD_RUN_DATABASE_SEED" = "true" ]; then
+  echo "Running database seeds..."
+  yarn seed
+fi
+
+
+if [ "$NODE_ENV" = "debug" ]; then
+  echo "Running in debug mode..."
+  yarn start:debug
+fi
+
+if [ "$NODE_ENV" = "local" ]; then
+  echo "Running in local mode..."
+  yarn start:local
+fi
+
+if [ "$NODE_ENV" = "dev" ]; then
+  echo "Running in dev mode..."
+  yarn start
+fi
+
+if [ "$NODE_ENV" = "stg" ]; then
+  echo "Running in stg mode..."
+  yarn start
+fi
+
+if [ "$NODE_ENV" = "prod" ]; then
+  echo "Running in prod mode..."
+  yarn start
+fi
