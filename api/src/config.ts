4656eb6bbcc00e0096ee3815ce4f7c627dfc697c
Mike <59815217+MFinn87@users.noreply.github.com>
2023-11-03 00:53:30 -0600

    Added Swagger documentation to API (#7)
    * Added Swagger documentation to API
    
    * Cleaned up api documentation verbiage

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,12 +1,55 @@
-const config = {
-  appOptions: {
-    logger: true,
-    bodyLimit: 209715200,
-  },
-  serverOptions: {
-    port: parseInt(process.env.API_PORT || '8080', 10),
-    host: '0.0.0.0',
-  }
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+const appOptions = {
+  logger: true,
+  bodyLimit: 209715200,
 }
 
-export default config
+const serverOptions = {
+  port: parseInt(process.env.API_PORT || '8080', 10),
+  host: '0.0.0.0',
+}
+
+const swaggerOptions = {
+  routePrefix: '/docs',
+  swagger: {
+    info: {
+      title: 'Urban Sky Interview Inventory API',
+      description: 'Urban Sky Interview Inventory API',
+      version: '1.0.0',
+    },
+    host: `${serverOptions.host}:${serverOptions.port}`,
+    basePath: '',
+    schemes: ['http', 'https'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    /*
+    tags: [{
+      name: 'Urban Sky Inventory API',
+      description: 'Urban Sky Inventory API',
+    },],
+    */
+    definitions: {},
+  },
+  uiConfig: {
+    docExpansion: 'list' as const, // expand/not all the documentations none|list|full
+    deepLinking: true
+  },
+  uiHooks: {
+    onRequest: (request: FastifyRequest, reply: FastifyReply, next: () => any) => {
+      next()
+    },
+    preHandler: (request: FastifyRequest, reply: FastifyReply, next: () => any) => {
+      next()
+    }
+  },
+  staticCSP: false,
+  transformStaticCSP: (header: any) => header,
+  exposeRoute: true,
+}
+
+export default {
+  appOptions,
+  serverOptions,
+  swaggerOptions,
+}
